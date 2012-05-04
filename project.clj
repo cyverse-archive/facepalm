f68ead8ad8e46e04a4a5aae1d9390abf60b5ab85
John Wregglesworth <wregglej@iplantcollaborative.org>
2012-05-04 14:32:49 -0700

    Initial commit of facepalm.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject facepalm "0.0.1-SNAPSHOT"
+  :description "Command-line utility for DE database managment."
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.cli "0.2.1"]])
