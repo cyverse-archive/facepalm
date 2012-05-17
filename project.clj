91c9202f2188c5956deb603f0b75ce3708718618
Dennis Roberts <dennis@iplantcollaborative.org>
2012-05-16 20:59:32 -0700

    CORE-3168: added code to parse command-line options and connect to the database

--- project.clj
+++ project.clj
@@ -1,4 +1,12 @@
 (defproject facepalm "0.0.1-SNAPSHOT"
   :description "Command-line utility for DE database managment."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]])
+                 [org.clojure/tools.cli "0.2.1"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [korma "0.3.0-beta7"]
+                 [org.iplantc/kameleon "0.0.1-SNAPSHOT"]
+                 [postgresql "9.0-801.jdbc4"]
+                 [slingshot "0.10.2"]]
+  :plugins [[lein-marginalia "0.7.0"]]
+  :aot [facepalm.core]
+  :main facepalm.core)
--- src/facepalm/core.clj
+++ src/facepalm/core.clj
@@ -1,1 +1,54 @@
-(ns facepalm.core)
+(ns facepalm.core
+  (:gen-class)
+  (:use [kameleon.core]
+        [kameleon.entities]
+        [kameleon.pgpass]
+        [korma.core]
+        [korma.db]
+        [clojure.tools.cli])
+  (:require [clojure.tools.logging :as log])
+  (:import [org.apache.log4j BasicConfigurator ConsoleAppender Level
+            SimpleLayout]))
+
+(defn- parse-args
+  "Parses the command-line arguments."
+  [args]
+  (cli args
+       ["-?" "--help" "Show help." :default false :flag true]
+       ["-h" "--host" "The database hostname." :default "localhost"]
+       ["-p" "--port" "The database port number." :default 5432
+        :parse-fn #(Integer. %)]
+       ["-d" "--database" "The database name." :default "de"]
+       ["-U" "--user" "The database username." :default "de"]
+       ["--debug" "Enable debugging." :default false :flag true]))
+
+(defn- configure-logging
+  "Configures logging for this tool.  All logging is printed on the console,
+   but the logging level may be changed."
+  [opts]
+  (BasicConfigurator/configure
+   (doto (ConsoleAppender.)
+     (.setLayout (SimpleLayout.))
+     (.setName "Console")
+     (.setThreshold (if (:debug opts) Level/DEBUG Level/INFO)))))
+
+(defn- define-db
+  "Defines the database connection settings."
+  [{:keys [host port database user]}]
+  (let [password (get-password host port database user)]
+    (defdb de {:classname "org.postgresql.Driver"
+               :subprotocol "postgresql"
+               :subname (str "//" host ":" port "/" database)
+               :user user
+               :password password})))
+
+(defn -main
+  "Parses the command-line options and performs the database updates."
+  [& args-vec]
+  (let [[opts args banner] (parse-args args-vec)]
+    (when (:help opts)
+      (println banner)
+      (System/exit 0))
+    (configure-logging opts)
+    (define-db opts)
+    (println (select users))))
