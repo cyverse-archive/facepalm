4c580ec5d49d398de6407a385d72fade38f69a75
Dennis Roberts <dennis@iplantcollaborative.org>
2014-01-31 17:56:17 -0700

    added some elements that are required by sonatype to project.clj

--- project.clj
+++ project.clj
@@ -6,6 +6,12 @@
 
 (defproject facepalm "2.0.0-SNAPSHOT"
   :description "Command-line utility for DE database managment."
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/facepalm.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/facepalm.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/facepalm.git"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.1"]
                  [org.clojure/tools.logging "0.2.3"]
@@ -13,7 +19,7 @@
                  [com.cemerick/pomegranate "0.0.13"]
                  [fleet "0.9.5"]
                  [korma "0.3.0-RC2"]
-                 [org.iplantc/clojure-commons "1.4.7"]
+                 [org.iplantc/clojure-commons "1.4.8-SNAPSHOT"]
                  [org.iplantc/kameleon "1.8.4-SNAPSHOT"]
                  [postgresql "9.0-801.jdbc4"]
                  [slingshot "0.10.3"]
@@ -25,5 +31,7 @@
                :type :command}
   :aot [facepalm.core]
   :main facepalm.core
-  :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
